9b6691735751c849de732b3b3a1637eea824beab
vthierry <thierry.vieville@inria.fr>
2013-06-10 16:28:10 +0200

    mise en place zero

---
+++ README.md
@@ -0,0 +1,5 @@
+Algo-Predateur
+========
+
+* [Le grain en action](http://inriamecsci.github.com/#!/grains/algo-predateur)
+
---
+++ js/preda.js
@@ -0,0 +1,410 @@
+$(function(){
+				
+				function dessinerAxes(){
+				$("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 1, y1: 1,
+				  x2: 10, y2: 1,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 10, y: 10,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "1"});
+				 $("#canvas2").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 1, y1: 1,
+				  x2: 10, y2: 1,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 10, y: 10,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "P"});
+				  $("canvas").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 1, y1: 240,
+				  x2: 10, y2: 240,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 10, y: 250,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "0.2"});
+				  $("canvas").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 1, y1: 180,
+				  x2: 10, y2: 180,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 10, y: 190,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "0.4"});
+				  $("canvas").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 1, y1: 120,
+				  x2: 10, y2: 120,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 10, y: 130,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "0.6"});
+				  $("canvas").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 1, y1: 60,
+				  x2: 10, y2: 60,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 10, y: 70,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "0.8"});
+				  $("canvas").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 1, y1: 1,
+				  x2: 10, y2: 1,});
+				  $("#canvas2").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 395, y1: 290,
+				  x2: 395, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 390, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "N"});
+				  $("#canvas2").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 320, y1: 290,
+				  x2: 320, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 330, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "0.8"});
+				   $("#canvas2").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 240, y1: 290,
+				  x2: 240, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 250, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "0.6"});
+				   $("#canvas2").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 160, y1: 290,
+				  x2: 160, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 170, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "0.4"});
+				  $("#canvas2").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 80, y1: 290,
+				  x2: 80, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 90, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "0.2"});
+				  $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 50, y1: 290,
+				  x2: 50, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 60, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "10"});
+				   $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 100, y1: 290,
+				  x2: 100, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 110, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "20"});
+				   $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 150, y1: 290,
+				  x2: 150, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 160, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "30"});
+				   $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 200, y1: 290,
+				  x2: 200, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 210, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "40"});
+				   $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 250, y1: 290,
+				  x2: 250, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 260, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "50"});
+				   $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 300, y1: 290,
+				  x2: 300, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 310, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "60"});
+				   $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 350, y1: 290,
+				  x2: 350, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 360, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "70"});
+				   $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 400, y1: 290,
+				  x2: 400, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 410, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "80"});
+				  $("#canvas1").drawLine({
+					strokeStyle: "black",
+				  strokeWidth: 1,
+				  rounded: true,
+				  x1: 450, y1: 290,
+				  x2: 450, y2: 300,}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 460, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "90"}).drawText({
+				   fillStyle: "#9cf",
+				  strokeStyle: "black",
+				  strokeWidth: 1,
+				  x: 490, y: 280,
+				  fontSize: "10pt",
+				  fontFamily: "sans-serif",
+				  text: "t"})
+				  
+				  }
+				  dessinerAxes();
+				  
+				// The .drawLine() object
+				var objP = {					//P (Lynx)
+				  strokeStyle: "green",
+				  strokeWidth: 1,
+				  rounded: true
+				};
+				
+				var objN = {				//N (Lièvre)
+				  strokeStyle: "red",
+				  strokeWidth: 1,
+				  rounded: true
+				};
+				
+				var objPP = {				//Portrait de phase
+				  strokeStyle: "blue",
+				  strokeWidth: 2,
+				  rounded: true
+				};
+				
+				
+				
+				function lancer(){
+				
+				//Paramètres
+				
+				r=parseFloat($("#rValue").val(),10);
+				w=parseFloat($("#wValue").val(),10);
+				e=parseFloat($("#eValue").val(),10);
+				k=parseFloat($("#kValue").val(),10);
+				m=parseFloat($("#mValue").val(),10);
+				p=parseFloat($("#pValue").val(),10);
+				n=parseFloat($("#nValue").val(),10);
+			
+				// Your array of points
+				var pts = new Array();
+				// Your array of points
+				var pts2 = new Array();
+				//Ajouter des points au tableau
+				for(i=0;i<500;i+=1){
+					
+					pts.push([]);
+					pts[i].push(i);					
+					pts[i].push(p);
+					pts2.push([]);
+					pts2[i].push(i);
+					pts2[i].push(n);
+					p=(((-m*p+e*w*n*p))*0.2+p);
+					n=((r*(1-n/k)*n-w*p*n)*0.2+n);
+					//n=Math.sin(i/10);
+					//p=Math.cos(i/10);					
+				}
+				function maximum(tableau){
+					var max=tableau[0][1];
+					for (var j=0;j<(tableau.length-1);j+=1){
+						if(max<tableau[j+1][1]){
+							max= tableau[j+1][1];
+							}
+						else{
+							
+							}						
+					}
+					return max;
+				}
+				maxi_p=maximum(pts);
+				//alert(maxi_p);
+				maxi_n=maximum(pts2);
+				//alert(maxi_n);
+				
+				// Add the points from the array to the object
+				
+				for (var p=0; p<pts.length; p+=1) {
+				
+				    objP['x'+(p+1)] = (pts[p][0]);
+				    
+				    objP['y'+(p+1)] = (1-(pts[p][1])/(maxi_p))*300;
+				}
+				
+			
+				
+				// Add the points from the array to the object
+				
+				for (var p=0; p<pts2.length; p+=1) {
+				
+					objN['x'+(p+1)] = pts2[p][0];
+					
+					objN['y'+(p+1)] = (1-(pts2[p][1])/(maxi_n))*300;
+					
+				}
+				
+				for (var p=0; p<(pts2.length/2); p+=1) {
+					
+					objPP['x'+(p+1)] = 300*(pts2[2*p][1]/maxi_n);
+					objPP['y'+(p+1)] = 300*(pts[2*p][1]/maxi_p);
+					objPP['cx'+(p+1)] = 300*(pts2[2*p+1][1]/maxi_n);
+					objPP['cy'+(p+1)] = 300*(pts[2*p+1][1]/maxi_p);
+					
+				}
+				
+				// Draw the line
+				$("#canvas1").drawLine(objP);
+				// Draw the line
+				$("#canvas1").drawLine(objN);
+				
+				$("#canvas2").drawQuadratic(objPP);
+				
+				}
+				
+				function effacer(){
+				
+					$("canvas").clearCanvas();
+					dessinerAxes();
+					$("canvas").drawImage({
+				  source: "legende.png",
+				  x: 400, y: 10,
+				  width: 80,
+				  height: 30,
+				  fromCenter: false
+				});
+				}
+		
+				$("canvas").drawImage({
+				  source: "legende.png",
+				  x: 400, y: 10,
+				  width: 80,
+				  height: 30,
+				  fromCenter: false
+				});
+				$("#lecture").on('click', lancer);
+				$("#effacer").on('click',effacer);
+		});
